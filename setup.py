23958df5b2ec909aaf40a62158fee9e867cfec1c
MickaelRigault <mick.rigault@gmail.com>
2023-02-16 14:51:05 +0100

    version up

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ MAINTAINER_EMAIL = "m.rigault@ipnl.in2p3.fr"
 URL = "https://github.com/MickaelRigault/ztflc/"
 LICENSE = "BSD (3-clause)"
 DOWNLOAD_URL = "https://github.com/MickaelRigault/ztflc"
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 
 try:
     from setuptools import setup, find_packages
